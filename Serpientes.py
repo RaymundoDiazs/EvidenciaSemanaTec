b68d432a402060fed145485ab9f322c6340b554a
RaymundoDiazs <73440290+RaymundoDiazs@users.noreply.github.com>
2023-05-13 21:05:31 -0600

    Add files via upload

---
+++ Serpientes.py
@@ -0,0 +1,78 @@
+from turtle import *
+from random import randrange
+from freegames import square, vector
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+
+
+color = ('red', 'green', 'purple','blue', 'yellow')
+
+begin_fill()
+
+while True:
+    snake_color = color[randrange(0, 5)]
+    food_color = color[randrange(0, 5)]
+    food_color = color[randrange(0, 5)]
+    break
+    end_fill()
+    done()
+
+
+
+def change(x, y):
+    "Change snake direction."
+    aim.x = x
+    aim.y = y
+
+def inside(head):
+    "Return True if head inside boundaries."
+    return -200 < head.x < 190 and -200 < head.y < 190
+
+def move():
+    "Move snake forward one segment."
+    head = snake[-1].copy()
+    head.move(aim)
+
+    #Mover comida
+    food.move(vector(randrange(-1, 2) * 10, randrange(-1, 2) * 10))
+
+    if not inside(food):
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
